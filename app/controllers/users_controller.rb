8ee7137946a2b5328ce8cc2b098c070026c738ab
rasovici <ivo.rasovic92@gmail.com>
2021-08-21 15:11:27 +0200

    controller pt1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.new
+  end
+
+
+private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:sport,:position,:gender,:footedness,:weight,:height,:description photos: [])
+  end
+
 end
