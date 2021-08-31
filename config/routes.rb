1daa28881d0fa301d256e27e792b54687e6fd632
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2021-08-31 21:40:51 +0200

    relationships-connections

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
   Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users
+  resources :users do
+    resources :follower_relationships, only: [:create, :index]
+  end
+
+  
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
