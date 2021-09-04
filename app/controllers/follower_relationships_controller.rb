6d90c918441932019302726e3f729ecda658b396
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2021-09-04 17:18:53 +0200

    sauvegarde

--- app/controllers/follower_relationships_controller.rb
+++ app/controllers/follower_relationships_controller.rb
@@ -4,11 +4,10 @@ def index
   @connections = (User.find(params[:user_id]).followed_users + User.find(params[:user_id]).following_users).uniq
 end
 
-
   def create
     @followed_user = User.find(params[:user_id])
     @follower_relationship = FollowerRelationship.new(user: current_user, followed_user: @followed_user)
-    @chatroom = Chatroom.new
+    @chatroom = Chatroom.new(name: "with #{@followed_user.first_name}")
     @follower_relationship.chatroom = @chatroom
     if @follower_relationship.save
       redirect_to user_follower_relationships_path(current_user)
