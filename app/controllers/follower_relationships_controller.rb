290b5248272292802215d69ad35bc73b628c4fbe
Michel Caluwaerts m.calu@skynet.be <m.calu@skynet.be>
2021-09-05 19:24:59 +0200

    access chatroom with message button in user's profil

--- app/controllers/follower_relationships_controller.rb
+++ app/controllers/follower_relationships_controller.rb
@@ -1,13 +1,13 @@
 class FollowerRelationshipsController < ApplicationController
-
-def index
+ 
+  def index
   @connections = (User.find(params[:user_id]).followed_users + User.find(params[:user_id]).following_users).uniq
 end
 
   def create
     @followed_user = User.find(params[:user_id])
     @follower_relationship = FollowerRelationship.new(user: current_user, followed_user: @followed_user)
-    @chatroom = Chatroom.new(name: "with #{@followed_user.first_name}")
+    @chatroom = Chatroom.new(name: "#{current_user.first_name} - #{@followed_user.first_name}")
     @follower_relationship.chatroom = @chatroom
     if @follower_relationship.save
       redirect_to user_follower_relationships_path(current_user)
@@ -15,4 +15,6 @@ end
       flash[:alert] = 'Sorry.'
     end
   end
+
+
 end
